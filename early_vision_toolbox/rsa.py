440ab3e082f35827be9d650849a1e8308796dcda
Yimeng Zhang <zym1010@gmail.com>
2016-06-01 10:06:11 -0400

    add batch rsa with nested loop, to deal with corentin's case

--- early_vision_toolbox/rsa.py
+++ early_vision_toolbox/rsa.py
@@ -16,10 +16,13 @@ def compute_rdm_list(X, split_k=2, split_rng_state=None, noise_level=None, noise
     :param rng_state:
     :return:
     """
+    assert split_k >= 2, "you can't you trivial split"
     assert X.ndim == 2, "you must give a image x neuron X array"
     if split_rng_state is None:
-        split_rng_state = np.random
+        split_rng_state = np.random.RandomState(None)
     rng_split_sets = np.array_split(split_rng_state.permutation(X.shape[1]), split_k)
+    # check the split is correct.
+    assert np.array_equal(np.unique(np.concatenate(rng_split_sets)), np.arange(X.shape[1]))
     rdm_list = []
     for split_set in rng_split_sets:
         response_mean_this = X[:, split_set]
@@ -35,7 +38,7 @@ def compute_rdm(X, noise_level=None, rng_state=None):
     if noise_level is None:
         noise_level = 0.0
     if rng_state is None:
-        rng_state = np.random  # use default generator
+        rng_state = np.random.RandomState(None)
     _X = X + noise_level * rng_state.randn(*X.shape) if noise_level != 0.0 else X
     result = pdist(_X, metric='correlation')
     assert np.all(np.isfinite(result)), "I can't allow invalid values in RDM"
@@ -61,15 +64,80 @@ def rdm_similarity(ref_rdms, rdm):
     return rdm_similarities
 
 
-def rdm_similarity_batch(ref_rdms, model_rdms, parallel=False, n_jobs=4, timing=True):
-    t = time.time()
+def rdm_similarity_batch(ref_rdms, model_rdms, parallel=False, n_jobs=4):
     if parallel:
         # disable memmap, due to <https://github.com/numpy/numpy/issues/6750>
         result = Parallel(n_jobs=n_jobs, verbose=5, max_nbytes=None)(
             delayed(rdm_similarity)(ref_rdms, rdm) for rdm in model_rdms)
     else:
         result = [rdm_similarity(ref_rdms, rdm) for rdm in model_rdms]
-    elapsed = time.time() - t
-    if timing:
-        print("job done in {} seconds".format(elapsed))
     return np.array(result)
+
+
+def rdm_similarity_batch_over_splits(ref_rdm_list, model_rdms, parallel=False, n_jobs=4,
+                                     split_k=2, n_iter=10,
+                                     split_rng_seeds=None, noise_levels=None, noise_rng_seeds=None):
+    """ do `split_k`-way split for `iter` times, and then compute mean and std in the first and second return values,
+    and return raw data in the third return value.
+
+    :param ref_rdm:
+    :param model_rdms:
+    :param parallel:
+    :param n_jobs:
+    :param split_k:
+    :param n_iter:
+    :param split_rng_seeds:
+    :param noise_levels:
+    :param noise_rng_seeds:
+    :return:
+    """
+    raw_array = []
+    n_ref_rdm_initial = len(ref_rdm_list)
+    if split_rng_seeds is None:
+        split_rng_seeds = [None] * n_iter
+    if noise_levels is None:
+        noise_levels = [None] * n_iter
+    if noise_rng_seeds is None:
+        noise_rng_seeds = [None] * n_iter
+    assert len(split_rng_seeds) == len(noise_levels) == len(noise_rng_seeds) == n_iter
+
+    for i in range(n_iter):
+        split_rng_seeds_this = split_rng_seeds[i]
+        noise_levels_this = noise_levels[i]
+        noise_rng_seeds_this = noise_rng_seeds[i]
+
+        if split_rng_seeds_this is None:
+            split_rng_seeds_this = [None] * n_ref_rdm_initial
+        if noise_levels_this is None:
+            noise_levels_this = [None] * n_ref_rdm_initial
+        if noise_rng_seeds_this is None:
+            noise_rng_seeds_this = [None] * n_ref_rdm_initial
+        assert len(split_rng_seeds_this) == len(noise_levels_this) == len(noise_rng_seeds_this) == n_ref_rdm_initial
+
+        ref_rdms = []
+        for j, ref_rdm in enumerate(ref_rdm_list):
+            ref_rdms.extend(
+                compute_rdm_list(ref_rdm, split_k, split_rng_state=np.random.RandomState(split_rng_seeds_this[j]),
+                                 noise_level=noise_levels_this[j],
+                                 noise_rng_state=np.random.RandomState(noise_rng_seeds_this[j])))
+        assert len(ref_rdms) == n_ref_rdm_initial*split_k
+        similarity_matrix_this = rdm_similarity_batch(ref_rdms, model_rdms, parallel=parallel, n_jobs=n_jobs)
+        raw_array.append(similarity_matrix_this)
+        print('done split {}/{}'.format(i+1, n_iter))
+
+    raw_array = np.array(raw_array)
+    print(raw_array.shape)
+    assert raw_array.shape == (n_iter, len(model_rdms), split_k*n_ref_rdm_initial)
+
+    similarity_array = np.mean(raw_array, axis=2)
+    assert similarity_array.shape == (n_iter, len(model_rdms))
+    mean_array = similarity_array.mean(axis=0)
+    std_array = similarity_array.std(axis=0)
+    assert mean_array.shape == std_array.shape == (len(model_rdms),)
+
+    # the correlation of mean similarity w.r.t different iters.
+    # should give len(model_rdms)*(len(model_rdms)-1)/2
+    correlation_matrix = 1-pdist(similarity_array.T, metric='correlation')
+    assert correlation_matrix.shape == (len(model_rdms)*(len(model_rdms)-1)//2, )
+
+    return mean_array, std_array, raw_array, correlation_matrix
