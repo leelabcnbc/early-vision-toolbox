a8e2b823a3f9b18559bec28c558f317c4dca5a71
Yimeng Zhang <zym1010@gmail.com>
2016-06-07 17:03:02 -0400

    add size verification

---
+++ early_vision_toolbox/cnn/size_util.py
@@ -0,0 +1,293 @@
+"""size utility module for CNN. basically a rewrite of my old code to compute any size-related thing to CNN"""
+
+from __future__ import division, print_function, absolute_import
+import numpy as np
+from collections import OrderedDict
+from copy import deepcopy
+
+
+def check_exact_cover(output_size, stride, kernelsize, input_size, pad_this):
+    assert (output_size[0] - 1) * stride + kernelsize == input_size[0] + 2 * pad_this, "input not exactly covered"
+    assert (output_size[1] - 1) * stride + kernelsize == input_size[1] + 2 * pad_this, "input not exactly covered"
+
+
+class CNNSizeHelper(object):
+    def __init__(self, layer_info_dict, input_size=None):
+        """
+
+        Parameters
+        ----------
+        layer_names
+        layer_strides
+        layer_kernelsizes
+        layer_pads
+        input_size
+        """
+        assert isinstance(layer_info_dict, OrderedDict)
+
+        num_layer = len(layer_info_dict)
+        for layer, value in layer_info_dict.iteritems():
+            assert {'pad', 'stride', 'kernelsize'} == set(value.keys())
+
+        # create "global" versions of layer_xxx variables, by computing layer by layer.
+        layer_strides_g = [None] * num_layer
+        layer_kernelsizes_g = [None] * num_layer
+        layer_pads_g = [None] * num_layer
+
+        result_dict = OrderedDict()
+        # input.
+        last_stride_g = 1
+        last_kernelsize_g = 1
+        last_pad_g = 0  # cummulative padding.
+        for layer, info_dict in layer_info_dict.iteritems():
+            # for each layer, I have to check that the units in the output blob
+            # can perfectly cover the units in the current blob (no uncovered units, nor redundant receptive field).
+            # This is crucial for unpooling, which only works for such case for now.
+            # In the long term, I think Caffe needs to implement deconv net itself.
+
+            # compute the stride, kernelsize, and pad for this layer, w.r.t. input layer.
+            stride_this = info_dict['stride']
+            kernelsize_this = info_dict['kernelsize']
+            pad_this = info_dict['pad']
+
+            pad_this_g = pad_this * last_stride_g + last_pad_g  # total padding in input layer
+            stride_this_g = stride_this * last_stride_g  # total stride in input layer.
+            kernelsize_this_g = (kernelsize_this - 1) * last_stride_g + last_kernelsize_g
+
+            info_dict_out_this = deepcopy(info_dict)
+            info_dict_out_this.update({
+                'stride_g': stride_this_g,
+                'kernelsize_g': kernelsize_this_g,
+                'pad_g': pad_this_g
+            })
+            result_dict[layer] = info_dict_out_this
+
+            last_stride_g = stride_this_g
+            last_kernelsize_g = kernelsize_this_g
+            last_pad_g = pad_this_g
+        self.layer_info_dict = result_dict
+
+        if input_size is not None:
+            self.compute_output_size(input_size)
+
+    @property
+    def output_size_dict(self):
+        """for some legacy tests"""
+        return {layer: self.layer_info_dict[layer]['output_size'] for layer in self.layer_info_dict}
+
+    @property
+    def field_size_dict(self):
+        """for some legacy tests"""
+        return {layer: self.layer_info_dict[layer]['kernelsize_g'] for layer in self.layer_info_dict}
+
+    @property
+    def stride_dict(self):
+        """for some legacy tests"""
+        return {layer: self.layer_info_dict[layer]['stride_g'] for layer in self.layer_info_dict}
+
+    @property
+    def pad_dict(self):
+        """for some legacy tests"""
+        return {layer: self.layer_info_dict[layer]['pad_g'] for layer in self.layer_info_dict}
+
+    def compute_output_size(self, input_size):
+        assert len(input_size) == 2, "you must specify both height and width"
+        last_input_size = input_size
+        for layer, info_dict_this in self.layer_info_dict.iteritems():
+            pad_this = info_dict_this['pad']
+            stride_this = info_dict_this['stride']
+            kernelsize_this = info_dict_this['kernelsize']
+
+            pad_this_g = info_dict_this['pad_g']
+            stride_this_g = info_dict_this['stride_g']
+            kernelsize_this_g = info_dict_this['kernelsize_g']
+
+            # compute the dimension of units for output.
+            output_size = ((last_input_size[0] + 2 * pad_this - kernelsize_this) // stride_this + 1,
+                           (last_input_size[1] + 2 * pad_this - kernelsize_this) // stride_this + 1)
+
+            # check that this output check can indeed exactly cover the input layer below,
+            check_exact_cover(output_size, stride_this, kernelsize_this, last_input_size, pad_this)
+            # as well as input image (plus padding).
+            check_exact_cover(output_size, stride_this_g, kernelsize_this_g, input_size, pad_this_g)
+            input_size_this = last_input_size
+            input_size_this_g = (input_size[0] + 2 * pad_this_g, input_size[1] + 2 * pad_this_g)
+
+            info_dict_this.update({
+                'output_size': output_size,
+                'input_size': input_size_this,
+                'input_size_g': input_size_this_g
+            })
+
+            last_input_size = output_size
+        self.input_size = input_size
+
+    def compute_valid_area(self, layer_name, row, col):
+        """ gives the indices in the input image space of one unit's receptive field.
+        :param layer_name:
+        :type layer_name: string
+        :param row:
+        :type row: int
+        :param col:
+        :type col: int
+        :return: (range_raw_r, range_raw_c, range_valid_r, range_valid_c). range_raw_r/c are raw indices (may contain
+        negative or too big values), and range_valid_r/c are the boolean mask of valid indices (True being valid).
+        :rtype:
+        """
+        layer_info_dict_this = self.layer_info_dict[layer_name]
+
+        field_size = layer_info_dict_this['kernelsize_g']
+        stride = layer_info_dict_this['stride_g']
+        pad = layer_info_dict_this['pad_g']
+        range_raw_r = np.arange(stride * row - pad, stride * row + field_size - pad).astype(np.int)
+        range_raw_c = np.arange(stride * col - pad, stride * col + field_size - pad).astype(np.int)
+        range_valid_r = np.logical_and(range_raw_r >= 0, range_raw_r < self.input_size[0])
+        range_valid_c = np.logical_and(range_raw_c >= 0, range_raw_c < self.input_size[1])
+
+        return range_raw_r, range_raw_c, range_valid_r, range_valid_c
+
+    def compute_inside_neuron(self, layer_name):
+        """
+            Compute inside neuron range
+            Inside neurons' receptive fields are in the image wholly.
+        :type projection: util.CaffeProjection
+        :param projection: projection for a convolutional network
+        :type layer_name: string
+        :param layer_name: layer's name
+        :rtype: tuple
+        :return: ranges for inside neurons
+        """
+        layer_info_dict_this = self.layer_info_dict[layer_name]
+
+        field_size = layer_info_dict_this['kernelsize_g']
+        stride = layer_info_dict_this['stride_g']
+        pad = layer_info_dict_this['pad_g']
+
+        input_rows = self.input_size[0]
+        input_cols = self.input_size[1]
+        row_range = (np.ceil(1.0 * pad / stride), np.floor(1.0 * (input_rows - field_size + pad) / stride))
+        col_range = (np.ceil(1.0 * pad / stride), np.floor(1.0 * (input_cols - field_size + pad) / stride))
+        if not (row_range[0] <= row_range[1] and col_range[0] <= col_range[1]):
+            raise ValueError('No inside neuron!')
+        return row_range[0], row_range[1] + 1, col_range[0], col_range[1] + 1
+
+    def compute_minimum_coverage(self, layer_name, top_left, bottom_right):
+        """ compute the miminum grid of neurons that can cover a rectangle with top left at top_left,
+        and bottom right at bottom_right
+
+        Parameters
+        ----------
+        layer_name
+        top_left
+        bottom_right
+
+        Returns
+        -------
+
+        """
+        top_pos, left_pos = top_left
+        bottom_pos, right_pos = bottom_right
+        assert 0 <= top_pos and top_pos <= bottom_pos and bottom_pos <= self.input_size[0]
+        assert 0 <= left_pos and left_pos <= right_pos and right_pos <= self.input_size[1]
+
+        # brutal force, find the four extreme neurons. It's fine, as we don't need performance here.
+        output_size_this_layer = self.layer_info_dict[layer_name]['output_size']
+        # get the coverage of all neurons.
+        row_idx, col_idx = np.meshgrid(np.arange(output_size_this_layer[0]), np.arange(output_size_this_layer[1]),
+                                       indexing='ij')
+        stride_g = self.layer_info_dict[layer_name]['stride_g']
+        pad_g = self.layer_info_dict[layer_name]['pad_g']
+        kernelsize_g = self.layer_info_dict[layer_name]['kernelsize_g']
+
+        min_r_all = stride_g * row_idx - pad_g
+        max_r_all = stride_g * row_idx - pad_g + kernelsize_g - 1
+
+        min_c_all = stride_g * col_idx - pad_g
+        max_c_all = stride_g * col_idx - pad_g + kernelsize_g - 1
+
+        mask_topleft = np.logical_and(np.logical_and(min_r_all <= top_pos, top_pos <= max_r_all),
+                                      np.logical_and(min_c_all <= left_pos, left_pos <= max_c_all))
+        top_left_location = row_idx[mask_topleft].max(), col_idx[mask_topleft].max()
+        assert mask_topleft[top_left_location]
+
+        mask_bottomright = np.logical_and(np.logical_and(min_r_all <= bottom_pos, bottom_pos <= max_r_all),
+                                          np.logical_and(min_c_all <= right_pos, right_pos <= max_c_all))
+        bottom_right_location = row_idx[mask_bottomright].min(), col_idx[mask_bottomright].min()
+        assert mask_bottomright[bottom_right_location]
+        return top_left_location, bottom_right_location
+
+
+layer_info_dict_raw = {
+    # in the order of layer name, stride, kernelsize, pad.
+    'vgg16': [('conv1_1', 1, 3, 1),
+              ('conv1_2', 1, 3, 1),
+              ('pool1', 2, 2, 0),
+              ('conv2_1', 1, 3, 1),
+              ('conv2_2', 1, 3, 1),
+              ('pool2', 2, 2, 0),
+              ('conv3_1', 1, 3, 1),
+              ('conv3_2', 1, 3, 1),
+              ('conv3_3', 1, 3, 1),
+              ('pool3', 2, 2, 0),
+              ('conv4_1', 1, 3, 1),
+              ('conv4_2', 1, 3, 1),
+              ('conv4_3', 1, 3, 1),
+              ('pool4', 2, 2, 0),
+              ('conv5_1', 1, 3, 1),
+              ('conv5_2', 1, 3, 1),
+              ('conv5_3', 1, 3, 1),
+              ('pool5', 2, 2, 0)],
+    'vgg19': [('conv1_1', 1, 3, 1),
+              ('conv1_2', 1, 3, 1),
+              ('pool1', 2, 2, 0),
+              ('conv2_1', 1, 3, 1),
+              ('conv2_2', 1, 3, 1),
+              ('pool2', 2, 2, 0),
+              ('conv3_1', 1, 3, 1),
+              ('conv3_2', 1, 3, 1),
+              ('conv3_3', 1, 3, 1),
+              ('conv3_4', 1, 3, 1),
+              ('pool3', 2, 2, 0),
+              ('conv4_1', 1, 3, 1),
+              ('conv4_2', 1, 3, 1),
+              ('conv4_3', 1, 3, 1),
+              ('conv4_4', 1, 3, 1),
+              ('pool4', 2, 2, 0),
+              ('conv5_1', 1, 3, 1),
+              ('conv5_2', 1, 3, 1),
+              ('conv5_3', 1, 3, 1),
+              ('conv5_4', 1, 3, 1),
+              ('pool5', 2, 2, 0)],
+    'alexnet': [('conv1', 4, 11, 0),
+                ('norm1', 1, 1, 0),
+                ('pool1', 2, 3, 0),
+                ('conv2', 1, 5, 2),
+                ('norm2', 1, 1, 0),
+                ('pool2', 2, 3, 0),
+                ('conv3', 1, 3, 1),
+                ('conv4', 1, 3, 1),
+                ('conv5', 1, 3, 1),
+                ('pool5', 2, 3, 0)]
+}
+
+
+def create_info_dict(info_dict_raw):
+    info_dict = OrderedDict()
+    for layer, stride, kernelsize, pad in info_dict_raw:
+        info_dict[layer] = {
+            'stride': stride,
+            'kernelsize': kernelsize,
+            'pad': pad
+        }
+    return info_dict
+
+
+layer_info_dict_dict = {key: create_info_dict(value) for key, value in layer_info_dict_raw.iteritems()}
+
+
+def create_size_helper(name_or_dict, input_size = None):
+    if isinstance(name_or_dict, str):
+        info_dict = deepcopy(layer_info_dict_dict[name_or_dict])
+    else:
+        info_dict = deepcopy(name_or_dict)
+    return CNNSizeHelper(info_dict, input_size)
--- tests/test_cnn.py
+++ tests/test_cnn.py
@@ -4,8 +4,10 @@ from early_vision_toolbox.cnn import caffe_models
 from early_vision_toolbox.cnn import dir_dict
 from early_vision_toolbox.cnn.network_definitions import caffe_deploy_proto_predefined, get_prototxt, net_info_dict
 from early_vision_toolbox.cnn.caffe_models import fill_weights, create_empty_net, create_predefined_net
+from early_vision_toolbox.cnn.size_util import create_size_helper
 from unittest import skip
 import numpy as np
+
 
 # TODO if we really want to be super sure, we should compile CPU caffe on travis and setup CI for this project.
 
@@ -36,7 +38,96 @@ class MyTestCase(unittest.TestCase):
                 for idx in range(len(param)):
                     self.assertTrue(np.array_equal(param[idx].data, net_ref.params[layer_name][idx].data))
 
+    def test_size_util_legacy(self):
+        """ original size related tests.
 
+        Returns
+        -------
+
+        """
+        # first, a legacy alexnet test
+        test_alexnet_projection()
+
+        # then, test the valid neuron stuff.
+        input_size_dict = {'alexnet': (227, 227),
+                           'vgg16': (224, 224),
+                           'vgg19': (224, 224)}
+
+        for model in input_size_dict:
+            print('test legacy model {}'.format(model))
+            #raw_input('wait to continue')
+            input_size = input_size_dict[model]
+            projection_this = create_size_helper(model, input_size=input_size)
+            test_valid_neurons(projection_this, list(projection_this.layer_info_dict.keys()))
+
+
+
+def test_valid_neurons(helper, layer_names_alex):
+    for layername in layer_names_alex:
+        try:
+            row_lower, row_upper, col_lower, col_upper = helper.compute_inside_neuron(layername)
+        except ValueError as e:
+            if e.args[0] == 'No inside neuron!':
+                print('this layer {} has no inside neuron'.format(layername))
+                #raw_input('wait to continue')
+                continue
+            else:
+                raise e
+        # check that these bounds are consistent with the ones computed by compute_valid_area
+        print(layername, row_lower, row_upper, col_lower, col_upper)
+        assert row_upper >= 1 and col_upper >= 1
+        assert row_lower >= 0 and col_lower >= 0
+        output_size_ref = helper.layer_info_dict[layername]['output_size']
+        assert row_upper <= output_size_ref[0] and col_upper <= output_size_ref[1]
+        # first check that neurons at (row_lower,col_lower) and (row_upper-1,col_upper-1) are with in RF.
+        _, _, range_valid_r, range_valid_c = helper.compute_valid_area(layername, row_lower, col_lower)
+        assert np.all(range_valid_r) and np.all(range_valid_c)
+
+        _, _, range_valid_r, range_valid_c = helper.compute_valid_area(layername, row_upper - 1, col_upper - 1)
+        assert np.all(range_valid_r) and np.all(range_valid_c)
+
+        # then check that as long as we move out of the boundaries a little bit, we get invalid pixels.
+        if row_lower > 0:
+            _, _, range_valid_r, range_valid_c = helper.compute_valid_area(layername, row_lower - 1, col_lower)
+            assert np.any(np.logical_not(range_valid_r)) and np.all(range_valid_c)
+
+        if col_lower > 0:
+            _, _, range_valid_r, range_valid_c = helper.compute_valid_area(layername, row_lower, col_lower - 1)
+            assert np.any(np.logical_not(range_valid_c)) and np.all(range_valid_r)
+
+        if row_upper < helper.output_size_dict[layername][0]:
+            _, _, range_valid_r, range_valid_c = helper.compute_valid_area(layername, row_upper, col_upper - 1)
+            assert np.any(np.logical_not(range_valid_r)) and np.all(range_valid_c)
+
+        if col_upper < helper.output_size_dict[layername][1]:
+            _, _, range_valid_r, range_valid_c = helper.compute_valid_area(layername, row_upper - 1, col_upper)
+            assert np.any(np.logical_not(range_valid_c)) and np.all(range_valid_r)
+
+
+def test_alexnet_projection():
+    print("this is a program to check that the dictionaries for info of layers can be computed from old info")
+    alex_net_projection = create_size_helper('alexnet', input_size=(227, 227))
+    field_size_dict = {'conv1': 11, 'pool1': 19, 'conv2': 51, 'pool2': 67, 'conv3': 99, 'conv4': 131, 'conv5': 163,
+                       'pool5': 195}
+    stride_dict = {'conv1': 4, 'pool1': 8, 'conv2': 8, 'pool2': 16, 'conv3': 16, 'conv4': 16, 'conv5': 16, 'pool5': 32}
+    pad_dict = {'conv1': 0, 'pool1': 0, 'conv2': 16, 'pool2': 16, 'conv3': 32, 'conv4': 48, 'conv5': 64, 'pool5': 64}
+    key_to_select = list(field_size_dict.keys())
+    # check they are the same.
+    assert {key: alex_net_projection.field_size_dict[key] for key in key_to_select} == field_size_dict
+    assert {key: alex_net_projection.stride_dict[key] for key in key_to_select} == stride_dict
+    assert {key: alex_net_projection.pad_dict[key] for key in key_to_select} == pad_dict
+
+    referenceDict = {'conv3': (13, 13),
+                     'conv2': (27, 27),
+                     'conv1': (55, 55),
+                     'conv5': (13, 13),
+                     'conv4': (13, 13),
+                     'pool2': (13, 13),
+                     'pool1': (27, 27),
+                     'pool5': (6, 6)}
+
+    assert {key: alex_net_projection.output_size_dict[key] for key in key_to_select} == referenceDict
+    print("old results reproduced by computation!")
 
 
 if __name__ == '__main__':
