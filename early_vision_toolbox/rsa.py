85d7a0e872f2e23313f0d7667b599a2a08d35ce8
Yimeng Zhang <zym1010@gmail.com>
2016-06-12 22:55:28 -0400

    refactor some RSA functions

--- early_vision_toolbox/rsa.py
+++ early_vision_toolbox/rsa.py
@@ -6,7 +6,7 @@ import numpy as np
 from scipy.stats import spearmanr
 from joblib import Parallel, delayed
 import time
-
+from scipy.stats import rankdata
 
 def compute_rdm_list(X, split_k=2, split_rng_state=None, noise_level=None, noise_rng_state=None, debug=False):
     """
@@ -32,6 +32,120 @@ def compute_rdm_list(X, split_k=2, split_rng_state=None, noise_level=None, noise
     final_result = np.array(rdm_list)
     assert final_result.ndim == 2 and final_result.shape[0] == split_k
     return final_result
+
+
+def compute_rdm_list_batch(ref_feature_matrix_list,
+                           split_k=2, n_iter=10,
+                           split_rng_seeds=None, noise_levels=None, noise_rng_seeds=None):
+    rdm_list_list = []
+    n_ref_rdm_initial = len(ref_feature_matrix_list)
+    if split_rng_seeds is None:
+        split_rng_seeds = [None] * n_iter
+    if noise_levels is None:
+        noise_levels = [None] * n_iter
+    if noise_rng_seeds is None:
+        noise_rng_seeds = [None] * n_iter
+    assert len(split_rng_seeds) == len(noise_levels) == len(noise_rng_seeds) == n_iter
+
+    for i in range(n_iter):
+        split_rng_seeds_this = split_rng_seeds[i]
+        noise_levels_this = noise_levels[i]
+        noise_rng_seeds_this = noise_rng_seeds[i]
+
+        if split_rng_seeds_this is None:
+            split_rng_seeds_this = [None] * n_ref_rdm_initial
+        if noise_levels_this is None:
+            noise_levels_this = [None] * n_ref_rdm_initial
+        if noise_rng_seeds_this is None:
+            noise_rng_seeds_this = [None] * n_ref_rdm_initial
+        assert len(split_rng_seeds_this) == len(noise_levels_this) == len(noise_rng_seeds_this) == n_ref_rdm_initial
+
+        ref_rdms = []
+        for j, ref_feature_matrix in enumerate(ref_feature_matrix_list):
+            # here it's extending a list with numpy array, and it will work as expected...
+            ref_rdms.extend(
+                compute_rdm_list(ref_feature_matrix, split_k,
+                                 split_rng_state=np.random.RandomState(split_rng_seeds_this[j]),
+                                 noise_level=noise_levels_this[j],
+                                 noise_rng_state=np.random.RandomState(noise_rng_seeds_this[j])))
+        assert len(ref_rdms) == n_ref_rdm_initial * split_k
+        rdm_list_list.append(ref_rdms)
+    return np.asarray(rdm_list_list)  # for compact.
+
+
+def compute_rdm_bounds(rdm_list, similarity_type='spearman'):
+    """computes the estimated lower and upper bounds of the similarity between the ideal model and the given data..
+    this is a remake of ``ceilingAvgRDMcorr`` in rsatoolbox in MATLAB.
+
+    Parameters
+    ----------
+    rdm_list
+    similarity_type : str
+        type of similarity. only spearman is supported currently.
+
+    Returns
+    -------
+
+    """
+    rdm_list = np.asarray(rdm_list)
+    assert rdm_list.ndim == 2
+    n_rdm = rdm_list.shape[0]
+    assert n_rdm >= 3, 'at least 3 RDMs to compute bounds (with 2, cross validation cannot be done)'
+    if similarity_type == 'spearman':
+        # transform everything to rank
+        rdm_list_rank = np.empty_like(rdm_list)
+        for idx, rdm in enumerate(rdm_list):
+            rdm_list_rank[idx] = rankdata(rdm)
+        best_rdm = rdm_list_rank.mean(axis=0)
+        upper_bound = rdm_similarity(rdm_list, best_rdm).mean()
+    else:
+        raise ValueError('supported similarity type!')
+
+    # compute lower bound. cross validation.
+    # maybe it's good to use np.bool_ rather than np.bool
+    # check <https://github.com/numba/numba/issues/1311>
+    all_true_vector = np.ones((n_rdm,), dtype=np.bool_)
+    similarity_list_for_lowerbound = []
+    for i_rdm in range(n_rdm):
+        selection_vector_this = all_true_vector.copy()
+        selection_vector_this[i_rdm] = False
+        similarity_list_for_lowerbound.append(spearmanr(rdm_list[selection_vector_this].mean(axis=0),
+                                                        rdm_list[i_rdm]).correlation)
+    lower_bound = np.array(similarity_list_for_lowerbound).mean()
+    return lower_bound, upper_bound
+
+
+def compute_rdm_bounds_batch(ref_rdms_list, similarity_type='spearman'):
+    """
+
+    Parameters
+    ----------
+    ref_feature_matrix_list
+    split_k
+    n_iter
+    split_rng_seeds
+    noise_levels
+    noise_rng_seeds
+    similarity_type
+
+    Returns
+    -------
+
+    """
+    lower_bound_array = []
+    upper_bound_array = []
+    n_iter = len(ref_rdms_list)
+
+    for i, ref_rdms in enumerate(ref_rdms_list):
+        lower_bound, upper_bound = compute_rdm_bounds(ref_rdms, similarity_type)
+        lower_bound_array.append(lower_bound)
+        upper_bound_array.append(upper_bound)
+
+    lower_bound_array = np.array(lower_bound_array)
+    upper_bound_array = np.array(upper_bound_array)
+    assert lower_bound_array.shape == upper_bound_array.shape == (n_iter,)
+
+    return lower_bound_array, upper_bound_array
 
 
 def compute_rdm(X, noise_level=None, rng_state=None):
@@ -76,9 +190,7 @@ def rdm_similarity_batch(ref_rdms, model_rdms, parallel=False, n_jobs=4):
     return np.array(result)
 
 
-def rdm_similarity_batch_over_splits(ref_rdm_list, model_rdms, parallel=False, n_jobs=4,
-                                     split_k=2, n_iter=10,
-                                     split_rng_seeds=None, noise_levels=None, noise_rng_seeds=None):
+def rdm_similarity_batch_over_splits(ref_rdms_list, model_rdms, parallel=False, n_jobs=4):
     """ do `split_k`-way split for `iter` times, and then compute mean and std in the first and second return values,
     and return raw data in the third return value.
 
@@ -86,50 +198,24 @@ def rdm_similarity_batch_over_splits(ref_rdm_list, model_rdms, parallel=False, n
     :param model_rdms:
     :param parallel:
     :param n_jobs:
-    :param split_k:
-    :param n_iter:
-    :param split_rng_seeds:
-    :param noise_levels:
-    :param noise_rng_seeds:
     :return:
     """
     raw_array = []
-    n_ref_rdm_initial = len(ref_rdm_list)
-    if split_rng_seeds is None:
-        split_rng_seeds = [None] * n_iter
-    if noise_levels is None:
-        noise_levels = [None] * n_iter
-    if noise_rng_seeds is None:
-        noise_rng_seeds = [None] * n_iter
-    assert len(split_rng_seeds) == len(noise_levels) == len(noise_rng_seeds) == n_iter
+    n_iter = len(ref_rdms_list)
+    n_total_rdm_each = None
 
-    for i in range(n_iter):
-        split_rng_seeds_this = split_rng_seeds[i]
-        noise_levels_this = noise_levels[i]
-        noise_rng_seeds_this = noise_rng_seeds[i]
-
-        if split_rng_seeds_this is None:
-            split_rng_seeds_this = [None] * n_ref_rdm_initial
-        if noise_levels_this is None:
-            noise_levels_this = [None] * n_ref_rdm_initial
-        if noise_rng_seeds_this is None:
-            noise_rng_seeds_this = [None] * n_ref_rdm_initial
-        assert len(split_rng_seeds_this) == len(noise_levels_this) == len(noise_rng_seeds_this) == n_ref_rdm_initial
-
-        ref_rdms = []
-        for j, ref_rdm in enumerate(ref_rdm_list):
-            ref_rdms.extend(
-                compute_rdm_list(ref_rdm, split_k, split_rng_state=np.random.RandomState(split_rng_seeds_this[j]),
-                                 noise_level=noise_levels_this[j],
-                                 noise_rng_state=np.random.RandomState(noise_rng_seeds_this[j])))
-        assert len(ref_rdms) == n_ref_rdm_initial*split_k
-        similarity_matrix_this = rdm_similarity_batch(ref_rdms, model_rdms, parallel=parallel, n_jobs=n_jobs)
+    for i, dataset in enumerate(ref_rdms_list):
+        if n_total_rdm_each is None:
+            n_total_rdm_each = len(dataset)
+        else:
+            assert n_total_rdm_each == len(dataset)
+        similarity_matrix_this = rdm_similarity_batch(dataset, model_rdms, parallel=parallel, n_jobs=n_jobs)
         raw_array.append(similarity_matrix_this)
-        print('done split {}/{}'.format(i+1, n_iter))
+        print('done split {}/{}'.format(i + 1, n_iter))
 
     raw_array = np.array(raw_array)
     print(raw_array.shape)
-    assert raw_array.shape == (n_iter, len(model_rdms), split_k*n_ref_rdm_initial)
+    assert raw_array.shape == (n_iter, len(model_rdms), n_total_rdm_each)
 
     similarity_array = np.mean(raw_array, axis=2)
     assert similarity_array.shape == (n_iter, len(model_rdms))
@@ -139,7 +225,7 @@ def rdm_similarity_batch_over_splits(ref_rdm_list, model_rdms, parallel=False, n
 
     # the correlation of mean similarity w.r.t different iters.
     # should give len(model_rdms)*(len(model_rdms)-1)/2
-    correlation_matrix = 1-pdist(similarity_array.T, metric='correlation')
-    assert correlation_matrix.shape == (len(model_rdms)*(len(model_rdms)-1)//2, )
+    correlation_matrix = 1 - pdist(similarity_array.T, metric='correlation')
+    assert correlation_matrix.shape == (len(model_rdms) * (len(model_rdms) - 1) // 2,)
 
     return mean_array, std_array, raw_array, correlation_matrix
